ee9869c16fbf4408a82bd571bd75ae22b89c31a2
Jim124 <fengpingan065@gmail.com>
2025-02-27 23:28:49 +0000

    set port

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -12,5 +14,10 @@ func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env file")
+		panic("could not load .env file")
 	}
+	port := os.Getenv("PORT")
+	gin.SetMode(gin.DebugMode)
+	server := gin.Default()
+	server.Run(fmt.Sprintf("localhost:%s", port))
 }
